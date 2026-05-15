5d16203c82ba5b1722aa0aef35309d35c55aa9e1
SujeethHR <sujithdx@gmail.com>
2026-05-15 14:10:34 +0530

    logger, exception, and utils added

---
+++ app.py
@@ -0,0 +1,4 @@
+import sys
+
+from book_recommender.logger.log import logging
+from book_recommender.exception.exception_handler import AppException
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="book_recommender",
+    version='0.1.0',
+    packages=find_packages(),
+    author='Sujeeth',
+    author_email='sujithdx@gmail.comn',
+    install_requires=[]
+)
